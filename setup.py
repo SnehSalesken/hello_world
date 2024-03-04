57b415ba518190b1bace791cf2d4b09b42063869
SnehSalesken <sneh@salesken.ai>
2024-03-04 12:40:13 +0530

    updated

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 setup(
     name='hello_world',
     version='0.0.1',
-    description='My private package from private github repo',
-    url='git@github.com:SnehSalesken/hello_world.git',
     author='Sneh Shah',
     license='unlicense',
 )
