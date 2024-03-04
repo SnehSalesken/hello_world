af6a3a5cb49aca6605436130ff3f18d02dbe2d19
SnehSalesken <sneh@salesken.ai>
2024-03-04 15:55:12 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(
-    name='hello_world',
-    version='0.0.1',
-    author='Sneh Shah'
-)
+setup(name='helloworld',
+      version='0.1',
+      url='https://github.com/SnehSalesken/hello_world.git',
+      author='Sneh Shah',
+      author_email='sneh@salesken.ai',
+      license='MIT',
+      zip_safe=False)
